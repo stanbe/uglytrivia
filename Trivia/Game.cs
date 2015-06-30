8571551ad53a5b08b253d15e4982180ce56ed772
Stefan Benz <stefan.benz@zuehlke.com>
2015-06-30 13:35:54 +0200

    extracted a QuestionPool

--- Trivia/Game.cs
+++ Trivia/Game.cs
@@ -1,37 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace Trivia
 {
     public class Game
     {
-        PlayerPool playerPool = new PlayerPool();
-
-        LinkedList<string> popQuestions = new LinkedList<string>();
-        LinkedList<string> scienceQuestions = new LinkedList<string>();
-        LinkedList<string> sportsQuestions = new LinkedList<string>();
-        LinkedList<string> rockQuestions = new LinkedList<string>();
+        private readonly PlayerPool playerPool = new PlayerPool();
+        private readonly QuestionPool questionPool = new QuestionPool();
 
         private readonly TextWriter outputWriter;
 
         public Game(TextWriter @out)
         {
             outputWriter = @out;
-
-            for (int i = 0; i < 50; i++)
-            {
-                popQuestions.AddLast("Pop Question " + i);
-                scienceQuestions.AddLast(("Science Question " + i));
-                sportsQuestions.AddLast(("Sports Question " + i));
-                rockQuestions.AddLast(CreateRockQuestion(i));
-            }
-        }
-
-        public String CreateRockQuestion(int index)
-        {
-            return "Rock Question " + index;
         }
 
         public bool Add(String playerName)
@@ -82,32 +63,14 @@ namespace Trivia
 
         private void AskQuestion()
         {
-            if (CurrentCategory() == QuestionCategory.Pop)
-            {
-                outputWriter.WriteLine(popQuestions.First());
-                popQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == QuestionCategory.Science)
-            {
-                outputWriter.WriteLine(scienceQuestions.First());
-                scienceQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == QuestionCategory.Sports)
-            {
-                outputWriter.WriteLine(sportsQuestions.First());
-                sportsQuestions.RemoveFirst();
-            }
-            if (CurrentCategory() == QuestionCategory.Rock)
-            {
-                outputWriter.WriteLine(rockQuestions.First());
-                rockQuestions.RemoveFirst();
-            }
+            var question = questionPool.GetQuestion(CurrentCategory());
+            outputWriter.WriteLine(question);
         }
 
         private QuestionCategory CurrentCategory()
         {
             var place = playerPool.CurrentPlayer.Place;
-            
+
             var result = QuestionCategory.Rock;
             if (place % 4 == 0) result = QuestionCategory.Pop;
             if (place % 4 == 1) result = QuestionCategory.Science;
@@ -143,7 +106,7 @@ namespace Trivia
         {
             outputWriter.WriteLine("Question was incorrectly answered");
             outputWriter.WriteLine(playerPool.CurrentPlayer.Name + " was sent to the penalty box");
-           playerPool.CurrentPlayer.PutInPenaltyBox();
+            playerPool.CurrentPlayer.PutInPenaltyBox();
 
             playerPool.NextPlayer();
             return true;
---
+++ Trivia/QuestionPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Trivia
+{
+    class QuestionPool
+    {
+        private readonly LinkedList<string> popQuestions = new LinkedList<string>();
+        private readonly LinkedList<string> scienceQuestions = new LinkedList<string>();
+        private readonly LinkedList<string> sportsQuestions = new LinkedList<string>();
+        private readonly LinkedList<string> rockQuestions = new LinkedList<string>();
+
+        public QuestionPool()
+        {
+            for (var i = 0; i < 50; i++)
+            {
+                popQuestions.AddLast("Pop Question " + i);
+                scienceQuestions.AddLast(("Science Question " + i));
+                sportsQuestions.AddLast(("Sports Question " + i));
+                rockQuestions.AddLast("Rock Question " + i);
+            }
+        }
+
+        public string GetQuestion(QuestionCategory category)
+        {
+            string question = null;
+            if (category == QuestionCategory.Pop)
+            {
+                question = popQuestions.First.Value;
+                popQuestions.RemoveFirst();
+            }
+            if (category == QuestionCategory.Science)
+            {
+                question = scienceQuestions.First.Value;
+                scienceQuestions.RemoveFirst();
+            }
+            if (category == QuestionCategory.Sports)
+            {
+                question = sportsQuestions.First.Value;
+                sportsQuestions.RemoveFirst();
+            }
+            if (category == QuestionCategory.Rock)
+            {
+                question = rockQuestions.First.Value;
+                rockQuestions.RemoveFirst();
+            }
+            return question;
+        }
+    }
+}
